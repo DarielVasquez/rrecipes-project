29fda4fcfc8452e715e94f4355e06d2fd8a08dc9
Dariel Vasquez <darielalejandro10@gmail.com>
2022-01-30 23:39:50 -0600

    changes to context

--- src/context.js
+++ src/context.js
@@ -2,6 +2,8 @@ import React, { useState, useContext, useEffect, useCallback } from "react";
 
 const url =
   "https://api.spoonacular.com/recipes/random?apiKey=0d31116822b54414a5fe84f683d6d5d9&number=13";
+const lowCaloriesUrl =
+  "https://api.spoonacular.com/recipes/findByNutrients?apiKey=0d31116822b54414a5fe84f683d6d5d9&minCalories=50&maxCalories=100";
 // const url =
 //   "https://api.spoonacular.com/recipes/complexSearch?apiKey=0d31116822b54414a5fe84f683d6d5d9";
 const AppContext = React.createContext();
@@ -9,17 +11,33 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [recipes, setRecipes] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const fetchData = async (url) => {
+    const response = await fetch(`${url}`);
+    const data = await response.json();
+    return data;
+  };
 
   const fetchRecipes = async () => {
     setLoading(true);
     try {
-      const response = await fetch(`${url}`);
-      const data = await response.json();
-      if (data.status === "failure") {
-        console.log(data.status);
+      //Fetch Random Recipes
+      const dataRandomRecipes = await fetchData(url);
+      //Fetch Low Calorie Recipes
+      const dataLowCalorieRecipes = await fetchData(lowCaloriesUrl);
+      if (dataRandomRecipes.status === "failure") {
+        console.log(dataRandomRecipes.status);
+        setLoading(false);
       } else {
-        const recipes = data.recipes;
-        setRecipes(recipes);
+        const randomRecipes = dataRandomRecipes.recipes;
+        setRecipes((prevRecipes) => {
+          return {
+            ...prevRecipes,
+            random: randomRecipes,
+            lowCalories: dataLowCalorieRecipes,
+          };
+        });
         setLoading(false);
       }
     } catch (error) {
@@ -32,7 +50,7 @@ const AppProvider = ({ children }) => {
   }, []);
 
   return (
-    <AppContext.Provider value={{ recipes, loading }}>
+    <AppContext.Provider value={{ recipes, loading, setSearchTerm }}>
       {children}
     </AppContext.Provider>
   );
