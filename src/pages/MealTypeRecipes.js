dad0d07b6bbb20aa91f314c4a85ca009044be325
Dariel Vasquez <darielalejandro10@gmail.com>
2022-03-13 23:55:53 -0600

    changes to meal types

--- src/pages/MealTypeRecipes.js
+++ src/pages/MealTypeRecipes.js
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
-import { useGlobalContext } from "../context";
 import Loading from "../components/Loading";
+import Score from "../components/Score";
+import { MdOutlineTimer } from "react-icons/md";
 
 const MealTypeRecipes = () => {
   const { meal } = useParams();
   const [loading, setLoading] = useState(true);
   const [recipeQuery, setRecipeQuery] = useState([]);
 
-  const url =
-    "https://api.spoonacular.com/recipes/complexSearch?apiKey=0d31116822b54414a5fe84f683d6d5d9&number=10&addRecipeInformation=true&&minCalories=0&maxCalories=25";
+  // const url =
+  //   "https://api.spoonacular.com/recipes/complexSearch?apiKey=0d31116822b54414a5fe84f683d6d5d9&number=10&addRecipeInformation=true&&minCalories=0&maxCalories=25";
 
   const fetchRecipe = async () => {
     try {
       const responseRecipes = await fetch(
-        `https://api.spoonacular.com/recipes/complexSearch?apiKey=0d31116822b54414a5fe84f683d6d5d9&number=20&addRecipeInformation=true&type=${meal}`
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=0d31116822b54414a5fe84f683d6d5d9&number=15&addRecipeInformation=true&type=${meal}`
       );
       const dataRecipes = await responseRecipes.json();
       const dataMealRecipes = dataRecipes.results;
@@ -24,7 +25,35 @@ const MealTypeRecipes = () => {
         setRecipeQuery(dataMealRecipes);
         setLoading(false);
       } else {
-        setRecipeQuery(dataMealRecipes);
+        switch (meal) {
+          case "main course":
+          case "side dish":
+          case "dessert":
+          case "appetizer":
+          case "salad":
+          case "bread":
+          case "breakfast":
+          case "soup":
+          case "beverage":
+          case "sauce":
+          case "marinade":
+          case "fingerfood":
+          case "snack":
+          case "drink":
+          case "antipasti":
+          case "brunch":
+          case "morning meal":
+          case "dip":
+          case "condiment":
+          case "starter":
+          case "hor d'oeuvre":
+          case "miscellaneous":
+            setRecipeQuery(dataMealRecipes);
+            break;
+          default:
+            setRecipeQuery([]);
+            break;
+        }
         setLoading(false);
       }
     } catch (error) {
@@ -34,25 +63,33 @@ const MealTypeRecipes = () => {
 
   useEffect(() => {
     fetchRecipe();
-  }, []);
+    return () => setRecipeQuery({});
+  }, [meal]);
 
   console.log(meal);
 
-  if (recipeQuery === undefined || recipeQuery.length === 0) {
-    return <div style={{ fontSize: "5em" }}>no recipes found</div>;
-  } else if (loading) {
+  if (loading) {
     return <Loading />;
-  } else
+  } else if (recipeQuery === undefined || recipeQuery.length === 0) {
+    return <div style={{ fontSize: "5em" }}>no recipes found</div>;
+  } else {
     return (
       <main>
         <div className="recipe-container">
           <section className="multiple-recipes">
-            <div>
+            <div className="recipes-title">
               <h1>{meal}</h1>
             </div>
             <div className="recipes-grid">
               {recipeQuery.slice(0, 3).map((recipe, index) => {
-                const { id, image, title } = recipe;
+                const {
+                  id,
+                  image,
+                  title,
+                  spoonacularScore,
+                  readyInMinutes,
+                  summary,
+                } = recipe;
                 return (
                   <div key={id} className={`box-item item-${index}`}>
                     <Link to={`/recipe/${id}`}>
@@ -62,6 +99,19 @@ const MealTypeRecipes = () => {
                       <div className={`box-body-${index}`}>
                         <div className="box-wrapper">
                           <h3 className="title-dish">{title}</h3>
+                        </div>
+                        <p
+                          className="recipes-summary"
+                          dangerouslySetInnerHTML={{
+                            __html: summary,
+                          }}
+                        ></p>
+                        <div className="timer">
+                          <span>
+                            <MdOutlineTimer />
+                            {readyInMinutes}mins
+                          </span>
+                          <Score score={spoonacularScore} />
                         </div>
                       </div>
                     </Link>
@@ -73,6 +123,7 @@ const MealTypeRecipes = () => {
         </div>
       </main>
     );
+  }
 };
 
 export default MealTypeRecipes;
